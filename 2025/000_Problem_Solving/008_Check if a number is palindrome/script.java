fca4a5b4ca5fc6e0a57d90afa437b586ad4c6a4b
Routh Kiran Babu <51410158+RouthKiranBabu@users.noreply.github.com>
2026-01-31 05:18:33 +0530

    Update script.java

--- 2025/000_Problem_Solving/008_Check if a number is palindrome/script.java
+++ 2025/000_Problem_Solving/008_Check if a number is palindrome/script.java
@@ -1,3 +1,33 @@
+public class Java{
+
+    public static void main(String[] args){
+        int number = 12321;
+
+        String str = String.valueOf(number);
+        int left = 0;
+        int right = str.length() - 1;
+
+        boolean isPalindrome = true;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                isPalindrome = false;
+                break;
+            }
+            left++;
+            right--;
+        }
+
+        if (isPalindrome) {
+            System.out.println(number + " is a palindrome");
+        } else {
+            System.out.println(number + " is not a palindrome");
+        }
+    }
+}
+
+// ------------------------------------------------------------------------------------------
+
 public class script {
     public static boolean isPalindrome(int num) {
         int original = num, reversed = 0;
@@ -14,3 +44,4 @@ public class script {
         System.out.println(num + " is palindrome? " + isPalindrome(num));
     }
 }
+
